ecd596271165253486ba937c7ddb02074379a288
max792489 <s1114777@o365st.pu.edu.tw>
2024-12-14 18:00:12 +0800

    [Feature] 修改 完成導覽列串接

--- VtuberWebsite/Controllers/NewsController.cs
+++ VtuberWebsite/Controllers/NewsController.cs
@@ -8,6 +8,10 @@ namespace VtuberWebsite.Controllers
 {
     public class NewsController : Controller
     {
+        public ActionResult Index()
+        {
+            return View("~/Views/Home/Index.cshtml");
+        }
         public ActionResult News()
         {
             return View();
