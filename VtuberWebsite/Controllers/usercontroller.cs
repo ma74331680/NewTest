06ae8514ff46dda96cfa7085b9b5f0d7e1921a49
max792489 <s1114777@o365st.pu.edu.tw>
2024-12-14 21:58:54 +0800

    [Feature] 修改 個人頭像

--- VtuberWebsite/Controllers/usercontroller.cs
+++ VtuberWebsite/Controllers/usercontroller.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
 
 namespace VtuberWebsite.Controllers
 {
@@ -23,6 +24,7 @@ namespace VtuberWebsite.Controllers
             return View();
         }
 
+        // 登入頁面
         public ActionResult Login()
         {
             return View();
@@ -32,5 +34,48 @@ namespace VtuberWebsite.Controllers
         {
             return View();
         }
+
+        // 登入提交
+        [HttpPost]
+        public ActionResult Login(string username, string password)
+        {
+            // 登入帳密驗證
+            if (username == "user" || password == "password") // 驗證邏輯
+            {
+                // 登入成功
+                FormsAuthentication.SetAuthCookie(username, false);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // 登入失敗
+                ViewBag.ErrorMessage = "登入失敗，請檢查用戶名或密碼。";
+                return View();
+            }
+        }
+        /*
+        // 註冊提交
+        [HttpPost]
+        public ActionResult Register(string username, string password, string confirmPassword)
+        {
+            if (password != confirmPassword)
+            {
+                ViewBag.ErrorMessage = "密碼不一致。";
+                return View();
+            }
+
+            // 註冊邏輯，例如新增至資料庫等
+
+            return RedirectToAction("Login");
+        }
+        */
+        /*
+        // 登出
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+        */
     }
 }
